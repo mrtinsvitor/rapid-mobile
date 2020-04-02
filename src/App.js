a2e0eec2c142aee36e10e25dbe9c26818b06ca1c
Vitor Martins <vitorfigmartins@gmail.com>
2020-04-02 20:12:53 -0300

    Adding FlashMessage dependency

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { ApplicationProvider as KittenProvider, IconRegistry } from '@ui-kitten/
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { mapping, light } from '@eva-design/eva';
+import FlashMessage from "react-native-flash-message";
 
 import MainNavigator from './router/MainNavigator';
 
@@ -18,6 +19,7 @@ const App = () => (
         <MainNavigator />
       </KittenProvider>
     </PaperProvider>
+    <FlashMessage position="bottom" titleStyle={{ fontFamily: 'Raleway-Regular', fontSize: 16 }} />
   </>
 );
 
